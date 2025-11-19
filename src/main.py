829484e6ffde03b5db991412ccdbb066390f58f6
Biswa Prakash <biswaprakash444@gmail.com>
2025-11-19 16:00:00 +0530

    feat: Add sample application for argocd preview environments

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Version": "1.0.0"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
